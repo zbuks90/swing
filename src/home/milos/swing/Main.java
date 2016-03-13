3ffe9fe6459f428ddcdb3b14dce91f6d18844bff
Milos Balas <balac.milos@yahoo.com>
2016-03-13 20:35:47 +0100

    Komentar: moj prvi commit

--- src/home/milos/swing/Main.java
+++ src/home/milos/swing/Main.java
@@ -1,10 +1,7 @@
 package home.milos.swing;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 
-import home.milos.swing.gui.MyFrame;
 import home.milos.swing.gui.home.StudentLogIn;
 
 public class Main {
